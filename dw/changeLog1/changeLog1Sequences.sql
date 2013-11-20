937bfa61985141d519d0466e99526707487223fc
Dave Steinich <drsteini@usgs.gov>
2013-11-20 17:38:31 +0000

    sequence for table suffix

--- dw/changeLog1/changeLog1Sequences.sql
+++ dw/changeLog1/changeLog1Sequences.sql
@@ -2,6 +2,6 @@
 
 --This is for the ars_stewards schema
 
---changeset drsteini:1SchemaSynonymsAA
+--changeset drsteini:1SchemaSequencesAA
 create sequence table_seq start with 1 maxvalue 99999 minvalue 1 cycle nocache noorder;
 --rollback drop sequence table_seq;
