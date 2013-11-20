c8fa96de607cfb3488bf6f71268736c5476617b5
Dave Steinich <drsteini@usgs.gov>
2013-11-20 17:35:43 +0000

    sequence for table suffix

---
+++ dw/changeLog1/changeLog1Sequences.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+
+--This is for the ars_stewards schema
+
+--changeset drsteini:1SchemaSynonymsAA
+create sequence table_seq start with 1 maxvalue 99999 minvalue 1 cycle nocache noorder;
+--rollback drop sequence table_seq;
