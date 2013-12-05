504369a77bc81197fca3e7c0e419ef7b59396919
Dave Steinich <drsteini@usgs.gov>
2013-12-05 14:06:10 +0000

    fix index create statement

--- staging/changeLog1/changeLog1Indexes.sql
+++ staging/changeLog1/changeLog1Indexes.sql
@@ -3,5 +3,5 @@
 --This is for the nwis_ws_star schema
 
 --changeset drsteini:1SchemaIndexesAA
-create index stewards_raw_xml_idx on stewards_raw_xml (raw_xml) indextype is xdbxmlindex parameters ('path table stewards_raw__path_table');
+create index stewards_raw_xml_idx on stewards_raw_xml (raw_xml) indextype is xmlindex parameters ('path table stewards_raw__path_table');
 --rollback drop index stewards_raw_xml_idx;
