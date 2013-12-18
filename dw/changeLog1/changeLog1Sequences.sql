a2d9bdb98ce52401340f26ec13e6c14fb8fb7b99
Dave Steinich <drsteini@usgs.gov>
2013-12-18 19:46:04 +0000

    let's do liquibase

--- dw/changeLog1/changeLog1Sequences.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---This is for the ars_stewards schema
-
---changeset drsteini:1SchemaSequencesAA
-create sequence table_seq start with 1 maxvalue 99999 minvalue 1 cycle nocache noorder;
---rollback drop sequence table_seq;
