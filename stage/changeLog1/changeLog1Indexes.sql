95870b54a3e1e2d7ae56f8a7cfca8f752c597a6b
Dave Steinich <drsteini@usgs.gov>
2014-02-10 18:11:55 +0000

    refactor etl for multiple result curls

---
+++ stage/changeLog1/changeLog1Indexes.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+
+--This is for the nwis_ws_star schema
+
+--changeset drsteini:1SchemaIndexesAA
+create index stewards_raw_xml_idx on stewards_raw_xml (raw_xml) indextype is xdb.xmlindex parameters ('path table stewards_raw__path_table');
+--rollback drop index stewards_raw_xml_idx;
