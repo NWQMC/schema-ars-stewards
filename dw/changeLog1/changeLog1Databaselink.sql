a61cf251d18b8e8797eca57682c40bde6061fea4
Dave Steinich <drsteini@usgs.gov>
2014-02-12 17:40:59 +0000

    WQP-299 - characteristic group

--- dw/changeLog1/changeLog1Databaselink.sql
+++ dw/changeLog1/changeLog1Databaselink.sql
@@ -3,5 +3,5 @@
 --This is for the ars_stewards schema
  
 --changeset drsteini:1SchemaDblinksAA
-create database link ars_stewards_${stage} connect to ars_stewards identified by "${stg_pwd}" using '${stage}';
---rollback drop database link ars_stewards_${stage};
+create database link ars_stewards_${stage}.er.usgs.gov connect to ars_stewards identified by "${stg_pwd}" using '${stage}.er.usgs.gov';
+--rollback drop database link ars_stewards_${stage}.er.usgs.gov;
