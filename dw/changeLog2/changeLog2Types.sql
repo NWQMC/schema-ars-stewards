d7de45944166ae050a8909673e761c680a59376d
David Steinich <drsteini@usgs.gov>
2015-04-07 09:51:07 -0500

    WQP-633

--- dw/changeLog2/changeLog2Types.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---This is for the ars_stewards schema
- 
---changeset drsteini:2SchemaTypesAA
-drop type typ_vctbl;
---rollback create or replace type typ_vctbl as table of varchar2(256);
